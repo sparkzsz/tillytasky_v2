525df63df159da3bf5626b9f0a870f3a9d8a4bfc
Chloe <newpsborder@gmail.com>
2026-08-30 11:35:55 -0500

    Update __root.tsx
    Add Apple favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -95,6 +95,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         href: appCss,
       },
       { rel: "icon", href: "/favicon.png", type: "image/png" },
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon_apple-touch-icon.png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
